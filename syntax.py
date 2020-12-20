d3ea462dd52676501c0fb24d96e7e1a6fce1a8a3
exitdump <sheikhforid1026@gmail.com>
2020-12-20 12:37:45 +0600

    basic syntax added

---
+++ syntax.py
@@ -0,0 +1,19 @@
+print('hello world!')
+
+#added to number
+
+a = 10
+b = 20
+sum = a + b
+print(sum)
+
+#input from user
+
+name = input("Enter your name : ")
+email = input("Enter your email address : ")
+print(name + " " + email)
+
+#Python Indentation
+
+if 5 > 2:
+    print("Five is greater than two!") 
